8ba06dadedc0c1dc9dfb480e8d2f1f7f3f3bd805
gxnie <wlsdl7520@naver.com>
2024-11-07 13:32:25 +0900

    init commit

---
+++ src/main/java/com/example/schedules_planner/SchedulesPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedules_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulesPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulesPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedules_planner/SchedulesPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedules_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulesPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
